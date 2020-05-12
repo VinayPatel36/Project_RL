f51cc553d5f501ddf0e253fcfe06c5e567ae937e
Vinay <vinay.rp.36@gmail.com>
2020-05-12 18:11:30 +0530

    Arguments added

--- Meta-RL/train.py
+++ Meta-RL/train.py
@@ -1,3 +1,50 @@
+#Handling command line args
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+#positional arguments
+parser.add_argument('env_name',help='The Environment name from gym')
+parser.add_argument('expected_reward',type=float , help='The reward at which Environment is considered solved')
+
+#optional arguments
+parser.add_argument('-ti','--train_iterations',type=int,help='Maximum training iterations')
+parser.add_argument('-mel','--max_episode_length',type=int,help='Maximum steps in each episode')
+parser.add_argument('-tbs','--trajectory_buffer_size',type=int,help='Trajectory Buffer Size')
+parser.add_argument('-bs','--batch_size',type=int,help='Batch size')
+parser.add_argument('-re','--render_every',type=int,help='Render every this many episodes')
+
+args = parser.parse_args()
+
+
+print(args)
+ENV_NAME = args.env_name
+EXPECTED_REWARD = args.expected_reward
+
+#ENV_NAME = sys.argv[1]
+TRAIN_ITERATIONS = 5000
+MAX_EPISODE_LENGTH = 1000
+TRAJECTORY_BUFFER_SIZE = 32
+BATCH_SIZE = 16
+RENDER_EVERY = 1
+#EXPECTED_REWARD = int(sys.argv[2])
+
+if args.train_iterations != None :
+    TRAIN_ITERATIONS = args.train_iterations
+if args.max_episode_length !=None :
+    MAX_EPISODE_LENGTH = args.max_episode_length
+if args.trajectory_buffer_size !=None :
+    TRAJECTORY_BUFFER_SIZE = args.trajectory_buffer_size
+if args.batch_size !=None :
+    BATCH_SIZE = args.batch_size
+if args.render_every !=None :
+    RENDER_EVERY = args.render_every
+
+
+#print('TRAIN_ITERATIONS',TRAIN_ITERATIONS,' MAX_EPISODE_LENGTH ',MAX_EPISODE_LENGTH)
+
+
 import numpy as np
 import os
 import time
@@ -9,13 +56,6 @@ from Agent import *
 import sys
 
 
-ENV_NAME = sys.argv[1]
-TRAIN_ITERATIONS = 5000
-MAX_EPISODE_LENGTH = 1000
-TRAJECTORY_BUFFER_SIZE = 32
-BATCH_SIZE = 16
-RENDER_EVERY = 1
-EXPECTED_REWARD = int(sys.argv[2])
 
 env = gym.make(ENV_NAME)
 agent = Agent(env.action_space.n,env.observation_space.shape,BATCH_SIZE)
